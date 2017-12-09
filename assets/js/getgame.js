84ba54dc938b73e8264383c0990bfbbbe13e43d9
hanifrfd <hanif.wrong@gmail.com>
2017-12-09 22:55:31 +0700

    infoGame finish !

--- assets/js/getgame.js
+++ assets/js/getgame.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var items = Array('navbar-warning', 'navbar-success', 'navbar-info', 'navbar-primary');
-	var random = items[Math.floor(Math.random() * items.length)]
+
 
 	$(".rand-nav-color").each(function() {
 		$(this).addClass(items.splice(~~(Math.random() * items.length), 1)[0]);
@@ -10,16 +10,13 @@ $(document).ready(function() {
 		infogame();
 	});
 
+	// release the game
+	pcgame();
+	ps4game();
+	androidgame();
+	nintedogame();
 
-	$('#hotgame').click(function() {
-		hotgame();
-	});
 
-	$('#clear').click(function() {
-		$('#div1').html('');
-		$('#div2').html('');
-		$('#div3').html('');
-	});
 	$('#example1').click(function() {
 		$('#div1').html('');
 		$('#div2').html('');
@@ -72,16 +69,18 @@ $(document).ready(function() {
 		});
 	});
 
-	function infogame() {
+	$.fn.myFunction = function(guid) {
+
+
 		$('#div1').html('');
 		$('#div2').html('');
 		$('#div3').html('');
-		var guid = '3030-23846';
+
 		$.ajax({
 			url: "https://www.giantbomb.com/api/game/" + guid + "/",
 			type: "get",
 			data: {
-				api_key: "ee6f3f25061cf71a97bd544052cf338657329c4e",
+				api_key: "d597c1525476b0216681ab7ff5eab1d11e44ff38",
 				format: "jsonp",
 				json_callback: "infogame_callback"
 			},
@@ -106,23 +105,164 @@ $(document).ready(function() {
 				$('#div1').append(msg);
 			},
 		});
+
+		// api key hanifr 'd597c1525476b0216681ab7ff5eab1d11e44ff38'
+		$.ajax({
+			url: "https://www.giantbomb.com/api/review/" + guid + "/",
+			type: "get",
+			data: {
+				api_key: "d597c1525476b0216681ab7ff5eab1d11e44ff38",
+				// field_list: "game,desription,reviewer",
+				format: "jsonp",
+				json_callback: "review"
+			},
+			dataType: "jsonp"
+		});
 	};
 
-	function hotgame() {
+
+	function pcgame() {
 		$('#div1').html('');
 		$('#div2').html('');
 		$('#div3').html('');
 		var searchs = 'assassins creed';
 		$.ajax({
-			url: "http://api.giantbomb.com/search/",
+			url: "https://www.giantbomb.com/api/games/",
+
 			type: "get",
 			data: {
-				api_key: "ee6f3f25061cf71a97bd544052cf338657329c4e",
-
-				query: searchs,
-				resources: "game",
+				api_key: "d597c1525476b0216681ab7ff5eab1d11e44ff38",
+				// field_list: "game,desription,reviewer",
+				filter: "platforms:94,expected_release_year:2016,expected_release_quarter:4",
+				sort: "date_added:desc",
+				limit: "20",
 				format: "jsonp",
-				json_callback: "example3"
+				json_callback: "PCGame"
+			},
+			dataType: "jsonp",
+			timeout: 5000,
+			error: function(jqXHR, exception) {
+				var msg = '';
+				if (jqXHR.status === 0) {
+					msg = 'Not connect.\n Verify Network.';
+				} else if (jqXHR.status == 404) {
+					msg = 'Requested page not found. [404]';
+				} else if (jqXHR.status == 500) {
+					msg = 'Internal Server Error [500].';
+				} else if (exception === 'parsererror') {
+					msg = 'Requested JSON parse failed.';
+				} else if (exception === 'timeout') {
+					msg = 'Time out error.';
+				} else if (exception === 'abort') {
+					msg = 'Ajax request aborted.';
+				} else {
+					msg = 'Uncaught Error.\n' + jqXHR.responseText;
+				}
+				$('#div3').append(msg);
+			},
+		});
+	};
+
+	function ps4game() {
+		$('#div1').html('');
+		$('#div2').html('');
+		$('#div3').html('');
+		var searchs = 'assassins creed';
+		$.ajax({
+			url: "https://www.giantbomb.com/api/games/",
+
+			type: "get",
+			data: {
+				api_key: "d597c1525476b0216681ab7ff5eab1d11e44ff38",
+				// field_list: "game,desription,reviewer",
+				filter: "platforms:146,expected_release_year:2016,expected_release_quarter:3",
+				sort: "date_added:desc",
+				limit: "20",
+				format: "jsonp",
+				json_callback: "PS4Game"
+			},
+			dataType: "jsonp",
+			timeout: 5000,
+			error: function(jqXHR, exception) {
+				var msg = '';
+				if (jqXHR.status === 0) {
+					msg = 'Not connect.\n Verify Network.';
+				} else if (jqXHR.status == 404) {
+					msg = 'Requested page not found. [404]';
+				} else if (jqXHR.status == 500) {
+					msg = 'Internal Server Error [500].';
+				} else if (exception === 'parsererror') {
+					msg = 'Requested JSON parse failed.';
+				} else if (exception === 'timeout') {
+					msg = 'Time out error.';
+				} else if (exception === 'abort') {
+					msg = 'Ajax request aborted.';
+				} else {
+					msg = 'Uncaught Error.\n' + jqXHR.responseText;
+				}
+				$('#div3').append(msg);
+			},
+		});
+	};
+
+	function androidgame() {
+		$('#div1').html('');
+		$('#div2').html('');
+		$('#div3').html('');
+		var searchs = 'assassins creed';
+		$.ajax({
+			url: "https://www.giantbomb.com/api/games/",
+
+			type: "get",
+			data: {
+				api_key: "d597c1525476b0216681ab7ff5eab1d11e44ff38",
+				// field_list: "game,desription,reviewer",
+				filter: "platforms:123,expected_release_year:2016,expected_release_quarter:3",
+				sort: "date_added:desc",
+				limit: "20",
+				format: "jsonp",
+				json_callback: "AndroidGame"
+			},
+			dataType: "jsonp",
+			timeout: 5000,
+			error: function(jqXHR, exception) {
+				var msg = '';
+				if (jqXHR.status === 0) {
+					msg = 'Not connect.\n Verify Network.';
+				} else if (jqXHR.status == 404) {
+					msg = 'Requested page not found. [404]';
+				} else if (jqXHR.status == 500) {
+					msg = 'Internal Server Error [500].';
+				} else if (exception === 'parsererror') {
+					msg = 'Requested JSON parse failed.';
+				} else if (exception === 'timeout') {
+					msg = 'Time out error.';
+				} else if (exception === 'abort') {
+					msg = 'Ajax request aborted.';
+				} else {
+					msg = 'Uncaught Error.\n' + jqXHR.responseText;
+				}
+				$('#div3').append(msg);
+			},
+		});
+	};
+
+	function nintedogame() {
+		$('#div1').html('');
+		$('#div2').html('');
+		$('#div3').html('');
+		$.ajax({
+			url: "https://www.giantbomb.com/api/games/",
+
+			type: "get",
+			data: {
+				api_key: "d597c1525476b0216681ab7ff5eab1d11e44ff38",
+				// field_list: "game,desription,reviewer",
+				filter: "platforms:117,expected_release_year:2016,expected_release_quarter:3",
+				sort: "date_added:desc",
+				limit: "20",
+				format: "jsonp",
+				json_callback: "NintendoGame"
 			},
 			dataType: "jsonp",
 			timeout: 5000,
@@ -187,7 +327,11 @@ function example2(data) {
 function infogame_callback(data) {
 	$('#info-namagame').append(data.results.name);
 	$('#publisher').append(data.results.publishers[0].name);
-	$('#platform').append(data.results.platforms[0].name);
+	for (var i = 0; i < data.results.platforms.length; i++) {
+		$('#platform').append(data.results.platforms[i].name);
+		$('#platform').append(',');
+	}
+
 	$('#info-desc').append(data.results.description);
 	$('#info-fotogame').append('<img src="' + data.results.image.original_url + '" alt="Rounded Image" class="img-rounded img-responsive">');
 
@@ -202,16 +346,41 @@ function infogame_callback(data) {
 	console.log(data);
 }
 
-function example3(data) {
-	// $('#div3').append('<h5> ' + data.results.name + '  </h5>');
-	// $('#div3').append('<h5> Description : </h5> ' + data.results.description);
-	// $('#div3').append('<h5> Publisher : </h5>' + data.results.publishers[0].name + '');
-	// $('#div3').append('<h5> Platform : </h5>' + data.results.platforms[0].name + '<br>');
-	// $('#div3').append('<h5> Screenshots : </h5> <br> ');
-	// // $('#div1').append('<h1> ' + data.results.images.length + '  </h1>')
+function PCGame(data) {
 	for (var i = 0; i < data.results.length; i++) {
-		$('#div3').append('<div class="col-sm-2 horizon-item"><img src="' + data.results[i].image.original_url + '" alt="Rounded Image" class="img-rounded" height="230" width="170"><div class="space-20"></div><div class=" col-sm-12 text-truncate text-center"><p>' + data.results[i].name + '<p></div></div>');
+		$('#PC-game').append('<div class="col-sm-2 horizon-item" style="overflow:hidden;"><a href="infogame.html?guid=' + data.results[i].guid + '"><img src="' + data.results[i].image.original_url + '" alt="Rounded Image" class="img-rounded" height="230" width="170"><div class="space-20"></div></a><div class="col-sm-12 text-truncate text-center" style="overflow-wrap:break-word;"><p>' + data.results[i].name + '<p></div></div>');
 	}
 	// $('#div3').append('<div class="space-30"></div>');
+	console.log(data);
+}
+
+function PS4Game(data) {
+	for (var i = 0; i < data.results.length; i++) {
+		$('#PS4-game').append('<div class="col-sm-2 horizon-item" style="overflow:hidden;><a href="infogame.html?guid=' + data.results[i].guid + '"><img src="' + data.results[i].image.original_url + '" alt="Rounded Image" class="img-rounded" height="230" width="170"><div class="space-20"></div></a><div class=" col-sm-12 text-truncate text-center"><p>' + data.results[i].name + '<p></div></div>');
+	}
+	// $('#div3').append('<div class="space-30"></div>');
+	console.log(data);
+}
+
+function AndroidGame(data) {
+	for (var i = 0; i < data.results.length; i++) {
+		$('#Android-Game').append('<div class="col-sm-2 horizon-item" style="overflow:hidden;><a href="infogame.html?guid=' + data.results[i].guid + '"><img src="' + data.results[i].image.original_url + '" alt="Rounded Image" class="img-rounded" height="230" width="170"><div class="space-20"></div></a><div class=" col-sm-12 text-truncate text-center"><p>' + data.results[i].name + '<p></div></div>');
+	}
+	// $('#div3').append('<div class="space-30"></div>');
+	console.log(data);
+}
+
+function NintendoGame(data) {
+	for (var i = 0; i < data.results.length; i++) {
+		$('#Nintendo-Game').append('<div class="col-sm-2 horizon-item" style="overflow:hidden;><a href="infogame.html?guid=' + data.results[i].guid + '"><img src="' + data.results[i].image.original_url + '" alt="Rounded Image" class="img-rounded" height="230" width="170"><div class="space-20"></div></a><div class=" col-sm-12 text-truncate text-center"><p>' + data.results[i].name + '<p></div></div>');
+	}
+	// $('#div3').append('<div class="space-30"></div>');
+	console.log(data);
+}
+
+function review(data) {
+	for (var i = 0; i < data.results.length; i++) {
+		$('#reviews').append('<div class="col-md-12"><nav class="navbar rand-nav-color"><div class="container"><div class="navbar-header">' + data.results[i].reviewer + '</div></div></nav><p>' + data.results[i].deck + '</p></div>');
+	}
 	console.log(data);
 }
