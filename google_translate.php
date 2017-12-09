5efb2634ac86b0fa6decb5bc7ac22f057ecf107c
jabbarsw <30877140+jabbarsw@users.noreply.github.com>
2017-12-09 15:48:48 +0700

    Komponen Translate

---
+++ google_translate.php
@@ -0,0 +1,5 @@
+<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'de,en,es,fr,id,pt', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
